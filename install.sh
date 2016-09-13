81f29238694d0711595b3057047beb46276bcbe2
Suraj S <suraj.s@cognizant.com>
2016-09-13 10:34:24 +0000

    Changed all files

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 wget -P Downloads/ https://svn.apache.org/repos/asf/oodt/tools/oodtsite.publisher/trunk/distribute_setup.py
 sudo python Downloads/distribute_setup.py
 sudo easy_install pip
+sudo apt-get install python-dev
 sudo pip install docker-compose
